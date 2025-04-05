ed9fad10678c78e4f96abacaf0f7e32d531e8b85
Kinga Szczawinska <kinga.zawarczynska@wilmaa.com>
2025-04-05 20:16:53 +0200

    feat(build): fix leaflet build issue

--- src/app/church/page.tsx
+++ src/app/church/page.tsx
@@ -1,14 +1,18 @@
-import bgImg from "../../assets/2.jpg";
-import MapComponent from "../components/map";
-import { WrapperWithBackground } from "../components/wrapper-with-bg";
+'use client';
+
+import bgImg from '../../assets/2.jpg';
+import { WrapperWithBackground } from '../components/wrapper-with-bg';
+
+import dynamic from 'next/dynamic';
+
+const MapComponent = dynamic(() => import('../components/map'), { ssr: false });
 
 export default function Church() {
-
   return (
     <WrapperWithBackground imgSrc={bgImg.src}>
-        <h1 className="text-white">KOŚCIÓŁ</h1>
-        <p className="text-white">info kościół</p>
-        <MapComponent />  
+      <h1 className="text-white">KOŚCIÓŁ</h1>
+      <p className="text-white">info kościół</p>
+      <MapComponent />
     </WrapperWithBackground>
   );
 }
