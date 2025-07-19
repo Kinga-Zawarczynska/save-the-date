f94fec0747b658876987c5865a033497788bdbbf
Kinga Szczawinska <kinga.zawarczynska@wilmaa.com>
2025-07-20 00:17:35 +0200

    feat(auth): remove console logs

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -6,8 +6,6 @@ export async function POST(req: Request) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { password }: { password: string } = await req.json();
   const storedHash = process.env.PASSWORD_HASH ?? '';
-  console.log('🚀 -> POST -> storedHash:', storedHash);
-  console.log('🚀 -> POST -> password:', password);
 
   if (!password || !(await bcrypt.compare(password, storedHash))) {
     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
